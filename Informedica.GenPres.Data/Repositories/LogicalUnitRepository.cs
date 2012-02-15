e345c7073b6157899739dae3cebfc947f720f70a
peterbartels <peterbartels@gmail.com>
2012-02-15 08:39:38 +0100

    Changed directory names to Informedica

---
+++ Informedica.GenPres.Data/Repositories/LogicalUnitRepository.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using GenPres.Business.Data.IRepositories;
+using GenPres.Business.Domain.Patients;
+using GenPres.Data.DAO.Mapper.Patients;
+using GenPres.Data.Managers;
+
+namespace GenPres.Data.Repositories
+{
+    public class LogicalUnitRepository : ILogicalUnitRepository
+    {
+        
+        private PdmsLogicalUnitMapper _logicalUnitMapper = new PdmsLogicalUnitMapper();
+        private XmlLogicalUnitMapper _xmlLogicalUnitMapper = new XmlLogicalUnitMapper();
+
+        public LogicalUnit[] GetLogicalUnits()
+        {
+            return GetLogicalUnitsByXml();
+        }
+
+        public LogicalUnit[] GetLogicalUnitsByXml()
+        {
+            return GetLogicalUnitsFromXml();
+        }
+
+        public LogicalUnit[] GetLogicalUnitsBySql()
+        {
+            return GetLogicalUnitsFromDatabase();
+        }
+
+        public LogicalUnit[] GetLogicalUnitsFromXml()
+        {
+            var xmlDoc = XDocument.Parse(Informedica.GenPres.Data.Properties.Resources.patients);
+            var logicalUnitsObj = from c in xmlDoc.Descendants("logicalunit")
+                    select new
+                    {
+                        name = c.Attribute("name").Value
+                    };
+
+            var logicalUnits = new LogicalUnit[logicalUnitsObj.Count()];
+
+            int id = 0;
+            foreach (var logicalunit in logicalUnitsObj)
+            {
+                logicalUnits[id] = _xmlLogicalUnitMapper.MapFromBoToDao(id, logicalunit.name, LogicalUnit.NewLogicalUnit());
+                id++;
+            }
+
+            return logicalUnits;
+        }
+
+        private LogicalUnit[] GetLogicalUnitsFromDatabase()
+        {
+            var sqlResult = PDMSDataRetriever.ExecuteSQL("SELECT * FROM LogicalUnits WHERE LogicalUnitID IN(1,50,57)");
+
+            LogicalUnit[] logicalUnits = new LogicalUnit[sqlResult.Tables[0].Rows.Count];
+
+            for (int i = 0; i < sqlResult.Tables[0].Rows.Count; i++)
+            {
+                logicalUnits[i] = _logicalUnitMapper.MapFromBoToDao(sqlResult.Tables[0].Rows[i], LogicalUnit.NewLogicalUnit());
+            }
+            return logicalUnits;
+        }
+    }
+}
---
+++ Informedica.GenPres.Settings/SecurityKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.Win32;
+
+namespace Settings
+{
+    public static class SecurityKey
+    {
+        public static string Key = "BusinessObjects2008";
+    }
+}
